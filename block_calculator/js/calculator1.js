919dd170b3beb7fe6c1437a31365b6686b1cbfa8
alzz-26 <56277823+alzz-26@users.noreply.github.com>
2020-01-14 02:07:57 +0530

    Add files via upload

---
+++ block_calculator/js/calculator1.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Js function for calculator.
+ */
+
+ (function ($, Drupal) {
+  Drupal.behaviors.block_calculator = {
+    attach: function (context, settings) {
+      $('.button').once('block_calculator').click(function () {
+      	var Num1 = parseInt(prompt('Enter the Larger Number'));
+      	var Num2 = parseInt(prompt('Enter the Smaller Number'));
+      	var Operator = prompt('Enter an operator(+,-,*,/)');
+
+      	if (Operator == '+') {
+      		alert(Num1 + Num2);
+      	}  else if (Operator == '-') {
+      		alert(Num1 - Num2);
+      	}  else if (Operator == '*') {
+      		alert(Num1 * Num2);
+      	}  else if (Operator == '/') {
+          alert(Num1 / Num2);
+        } else {
+          alert('Operation not supported');
+        }    	
+      });
+    }
+  };
+})(jQuery, Drupal);
